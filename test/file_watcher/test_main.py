49fa0d8f24cf57baafab0adddc6057cff5caea10
Keiran Price <keiran.price@stfc.ac.uk>
2025-05-01 08:07:02 +0100

    Refactor and standardize code formatting and imports

--- test/file_watcher/test_main.py
+++ test/file_watcher/test_main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from unittest import mock
 from unittest.mock import patch
 
-from file_watcher.main import load_config, FileWatcher, main
+from file_watcher.main import FileWatcher, load_config, main
 from test.file_watcher.utils import AwaitableNonAsyncMagicMock
 
 
@@ -16,16 +16,16 @@ class MainTest(unittest.TestCase):
     def test_load_config_defaults(self):
         config = load_config()
 
-        self.assertEqual(config.host, "localhost")
-        self.assertEqual(config.username, "guest")
-        self.assertEqual(config.password, "guest")
-        self.assertEqual(config.queue_name, "watched-files")
-        self.assertEqual(config.watch_dir, Path("/archive"))
-        self.assertEqual(config.run_file_prefix, "MAR")
-        self.assertEqual(config.instrument_folder, "NDXMARI")
-        self.assertEqual(config.db_ip, "localhost")
-        self.assertEqual(config.db_username, "admin")
-        self.assertEqual(config.db_password, "admin")
+        assert config.host == "localhost"
+        assert config.username == "guest"
+        assert config.password == "guest"  # noqa: S105
+        assert config.queue_name == "watched-files"
+        assert config.watch_dir == Path("/archive")
+        assert config.run_file_prefix == "MAR"
+        assert config.instrument_folder == "NDXMARI"
+        assert config.db_ip == "localhost"
+        assert config.db_username == "admin"
+        assert config.db_password == "admin"  # noqa: S105
 
     def test_load_config_environ_vars(self):
         host = str(mock.MagicMock())
@@ -52,15 +52,15 @@ class MainTest(unittest.TestCase):
 
         config = load_config()
 
-        self.assertEqual(config.host, host)
-        self.assertEqual(config.username, username)
-        self.assertEqual(config.password, password)
-        self.assertEqual(config.watch_dir, Path(watch_dir))
-        self.assertEqual(config.run_file_prefix, run_file_prefix)
-        self.assertEqual(config.instrument_folder, instrument_folder)
-        self.assertEqual(config.db_ip, db_ip)
-        self.assertEqual(config.db_username, db_username)
-        self.assertEqual(config.db_password, db_password)
+        assert config.host == host
+        assert config.username == username
+        assert config.password == password
+        assert config.watch_dir == Path(watch_dir)
+        assert config.run_file_prefix == run_file_prefix
+        assert config.instrument_folder == instrument_folder
+        assert config.db_ip == db_ip
+        assert config.db_username == db_username
+        assert config.db_password == db_password
 
     @patch("file_watcher.main.FileWatcher.producer_channel")
     def test_file_watcher_on_event_produces_message(self, mock_producer):
@@ -86,10 +86,8 @@ class MainTest(unittest.TestCase):
         with tempfile.TemporaryDirectory() as tmpdirname:
             path = Path(tmpdirname)
             self.file_watcher.on_event(path)
-            logger.info.assert_called_with(
-                "Skipping directory creation for %s", tmpdirname
-            )
-            self.assertEqual(logger.info.call_count, 1)
+            logger.info.assert_called_with("Skipping directory creation for %s", tmpdirname)
+            assert logger.info.call_count == 1
         mock_producer.assert_not_called()
 
     @patch("file_watcher.main.write_readiness_probe_file")
@@ -107,8 +105,8 @@ class MainTest(unittest.TestCase):
         def raise_exception(callback_func):
             raise exception
 
-        mock_create_last_run_detector.return_value.watch_for_new_runs = (
-            AwaitableNonAsyncMagicMock(side_effect=raise_exception)
+        mock_create_last_run_detector.return_value.watch_for_new_runs = AwaitableNonAsyncMagicMock(
+            side_effect=raise_exception
         )
 
         # Should not raise, if raised it does not handle exceptions correctly
@@ -117,20 +115,14 @@ class MainTest(unittest.TestCase):
         mock_create_last_run_detector.return_value.watch_for_new_runs.assert_called_once_with(
             callback_func=mock_write_readiness_probe_file
         )
-        mock_logger.info.assert_called_with(
-            "File observer fell over watching because of the following exception:"
-        )
+        mock_logger.info.assert_called_with("File observer fell over watching because of the following exception:")
         mock_logger.exception.assert_called_with(exception)
 
     @patch("file_watcher.main.create_last_run_detector")
-    def test_file_watcher_start_watching_creates_last_run_detector(
-        self, mock_create_last_run_detector
-    ):
+    def test_file_watcher_start_watching_creates_last_run_detector(self, mock_create_last_run_detector):
         self.file_watcher = FileWatcher(self.config)
 
-        with mock.patch(
-            "file_watcher.main.write_readiness_probe_file"
-        ) as write_readiness_probe_file_mock:
+        with mock.patch("file_watcher.main.write_readiness_probe_file") as write_readiness_probe_file_mock:
             self.file_watcher.start_watching()
 
         mock_create_last_run_detector.return_value.watch_for_new_runs.assert_called_once_with(
@@ -151,12 +143,8 @@ class MainTest(unittest.TestCase):
     def test_channel_producer(self, mock_connection, mock_conn_params, mock_creds):
         channel = mock_connection.return_value.channel.return_value
         with FileWatcher(self.config).producer_channel():
-            mock_creds.assert_called_once_with(
-                username=self.config.username, password=self.config.password
-            )
-            mock_conn_params.assert_called_once_with(
-                self.config.host, 5672, credentials=mock_creds.return_value
-            )
+            mock_creds.assert_called_once_with(username=self.config.username, password=self.config.password)
+            mock_conn_params.assert_called_once_with(self.config.host, 5672, credentials=mock_creds.return_value)
             mock_connection.assert_called_once_with(mock_conn_params.return_value)
             channel.exchange_declare.assert_called_once_with(
                 self.config.queue_name, exchange_type="direct", durable=True
@@ -166,8 +154,6 @@ class MainTest(unittest.TestCase):
                 durable=True,
                 arguments={"x-queue-type": "quorum"},
             )
-            channel.queue_bind.assert_called_once_with(
-                self.config.queue_name, self.config.queue_name, routing_key=""
-            )
+            channel.queue_bind.assert_called_once_with(self.config.queue_name, self.config.queue_name, routing_key="")
         channel.close.assert_called_once()
         channel.connection.close.assert_called_once()
